f26e63912e7843c1f4f7b2edc84a638457735183
jiang <jiang@jiangdeMac-mini.local>
2025-01-01 16:53:51 +0800

    Integrate Leadsnavi

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,8 @@ import { FooterSection } from "@/components/layout/sections/footer";
 const inter = Inter({ subsets: ["latin"] });
 import { CookieConsent } from "@/components/cookie-consent/cookie-consent";
 import { GoogleAnalytics } from '@next/third-parties/google'
+import Script from 'next/script'
+
 
 // export const metadata: Metadata = {
 //   title: "Grimo - Reimagine Word Processor",
@@ -30,6 +32,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="pt-br" suppressHydrationWarning>
+      <head>
+        <Script id="track-sdk" strategy="afterInteractive">
+          {`(function(siteId){window.__track_sdk__=window.__track_sdk__||{temp:[],report:function(){this.temp.push([].slice.call(arguments))},};(function(doc,tagName){var ele=doc.getElementsByTagName(tagName)[0];function onLoad(){if(window.__track_sdk__){window.__track_sdk__.setDefaultConfig({siteId:siteId,})}}function insert(){var s=document.createElement('script');s.type='text/javascript';s.async=true;s.src='https://vr.leadsnavi.com/track-sdk.js';s.onload=onLoad;ele.parentNode.insertBefore(s,ele)}insert()})(document,'script')})('5296e5ad53b64d5ab1dc705d1985713c')`}
+        </Script>
+      </head>
       <GoogleAnalytics gaId="GTM-TQVNTXZ8" />
       <body className={cn("min-h-screen bg-background", inter.className)}>
         <ThemeProvider
