5fd778f8fc2348d22fb21833a4ad764cbffeef41
jiang <jiang@jiangdeMac-mini.local>
2025-04-07 12:05:09 +0800

    http-->https

--- components/layout/sections/hero.tsx
+++ components/layout/sections/hero.tsx
@@ -113,7 +113,7 @@ export const HeroSection = () => {
             <Button asChild className=" w-40 font-bold group/arrow rounded-[0.4rem]">
               <Link
                 aria-label="Build Your Words"
-                href="http://beta.grimo.ai"
+                href="https://beta.grimo.ai"
                 target="_blank"
               >
                 Build Your Words
