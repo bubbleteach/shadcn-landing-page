d50971a12859ef9829efee8c38f70666e90cbb21
jiang <jiang@jiangdeMac-mini.local>
2025-04-09 20:14:49 +0800

    added releases

---
+++ components/ui/ReleaseContent.tsx
@@ -0,0 +1,23 @@
+'use client'
+import { useState, useEffect, useTransition } from "react";
+import markdownToHtml from "../../lib/markdownToHtml"
+
+export default function ReleaseContent({ content }: { content: string }) {
+    const [htmlContent, setHtmlContent] = useState('');
+    const [isPending, startTransition] = useTransition();
+  
+    useEffect(() => {
+      startTransition(async () => {
+        const html = await markdownToHtml(content || "");
+        setHtmlContent(html);
+      });
+    }, [content]);
+  
+    if (isPending) {
+      return <div>Loading...</div>;
+    }
+  
+    return <div className="prose">
+        <div dangerouslySetInnerHTML={{ __html: htmlContent }} />
+    </div>
+  }
